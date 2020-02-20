d17fe64a86e1ec025e4948b373aa186fc79ce8dc
Gregory W. Schwartz <gsch@pennmedicine.upenn.edu>
2020-02-20 10:45:33 -0500

    Allow for Min / Max identification.

--- src/Math/Elbow.hs
+++ src/Math/Elbow.hs
@@ -10,6 +10,7 @@ module Math.Elbow
   ( findElbow
   , findElbowList
   , getRotationAngle
+  , MaxMin (..)
   ) where
 
 -- Remote
@@ -17,6 +18,9 @@ import Safe (headMay, atMay)
 import qualified Numeric.LinearAlgebra as H
 
 -- Local
+
+
+data MaxMin = Max | Min
 
 -- | Convert a list to a tuple.
 listToTuple :: [a] -> Maybe (a, a)
@@ -32,22 +36,30 @@ getRotationAngle = fmap (uncurry atan2)
                  . fmap (\x -> H.maxElement x - H.minElement x)
                  . H.toColumns
 
--- | Get the elbow point (and its index) of a two-dimensional distribution.
--- Matrix rows are observations, columns are dimensions.
-findElbow :: (RealFloat a, H.Numeric a) => H.Matrix a -> Maybe (Int, (a, a))
-findElbow m = do
+-- | Get the elbow point (and its index) of a two-dimensional distribution. Uses
+-- the Max or Min to identify convex / concave point, depending on the choice
+-- (for positive x and y, top left hump would be Max, bottom right dip would be
+-- Min). Matrix rows are observations, columns are dimensions.
+findElbow :: (RealFloat a, H.Numeric a)
+          => MaxMin -> H.Matrix a -> Maybe (Int, (a, a))
+findElbow maxMin m = do
   theta <- getRotationAngle m
 
   let co = cos theta
       si = sin theta
       rot = (2 H.>< 2) [co, -si, si, co]
       rotated = m H.<> rot
+      findMaxMin Max = H.maxIndex
+      findMaxMin Min = H.minIndex
 
-  idx <- fmap H.minIndex . flip atMay 1 . H.toColumns $ rotated
+  idx <- fmap (findMaxMin maxMin) . flip atMay 1 . H.toColumns $ rotated
 
   fmap (idx,) . listToTuple . H.toList $ m H.! idx
 
--- | Get the elbow point (and its index) of a two-dimensional distribution.
--- Matrix rows are observations, columns are dimensions. List of columns.
-findElbowList :: (RealFloat a, H.Numeric a) => [[a]] -> Maybe (Int, (a, a))
-findElbowList = findElbow . H.fromColumns . fmap H.fromList
+-- | Get the elbow point (and its index) of a two-dimensional distribution. Uses
+-- the Max or Min to identify convex / concave point, depending on the choice
+-- (for positive x and y, top left hump would be Max, bottom right dip would be
+-- Min). Matrix rows are observations, columns are dimensions. List of columns.
+findElbowList :: (RealFloat a, H.Numeric a)
+              => MaxMin -> [[a]] -> Maybe (Int, (a, a))
+findElbowList maxMin = findElbow maxMin . H.fromColumns . fmap H.fromList
